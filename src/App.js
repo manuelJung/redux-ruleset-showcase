e32419e02f2200e09d498c1623be3b882557d619
manuelJung <manu.jung@relaxcommerce.com>
2020-04-06 21:22:31 +0200

    added hilight.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import * as React from 'react'
+import 'highlight.js/styles/github.css'
+import hljs from 'highlight.js/lib/highlight'
+import javascript from 'highlight.js/lib/languages/javascript'
+hljs.registerLanguage('javascript', javascript)
 
 export default function App() {
+  const code = React.useRef()
+  React.useEffect(() => {
+    if(!code.current) return
+    hljs.highlightBlock(code.current)
+  })
   return (
     <div className="App">
       Hello world
+      <pre><code ref={code} className="language-javascript">
+{`// foo
+console.log('hello world')
+`}
+        </code></pre>
     </div>
   )
 }
