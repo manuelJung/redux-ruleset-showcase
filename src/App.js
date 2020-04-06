1545e819d7be85a9c9c83793801a51efbd6a113b
manuelJung <manu.jung@relaxcommerce.com>
2020-04-06 23:18:32 +0200

    setup reach/router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import * as React from 'react'
-import 'highlight.js/styles/github.css'
-import hljs from 'highlight.js/lib/highlight'
-import javascript from 'highlight.js/lib/languages/javascript'
-import UserList from './components/UserList'
-import Code from './components/Code'
-hljs.registerLanguage('javascript', javascript)
+import { Router } from "@reach/router"
 
-const demoCode= `
-// comment
-console.log('foo)
-`
+import IndexRoute from './routes/index'
 
 export default function App() {
-  const code = React.useRef()
-  React.useEffect(() => {
-    if(!code.current) return
-    hljs.highlightBlock(code.current)
-  })
   return (
     <div className="App">
-      <Code content={demoCode}/>
-      <UserList/>
+      <Router>
+        <IndexRoute path='/'/>
+      </Router>
     </div>
   )
 }
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+export default function IndexRoute () {
+  return (
+    <Wrapper>
+      Index Route
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div``
