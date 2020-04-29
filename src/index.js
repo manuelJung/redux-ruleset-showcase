d43ac56d8c38ac0878327573a97744873b75f7d6
manuelJung <manu.jung@relaxcommerce.com>
2020-04-29 23:29:07 +0200

    wrap with Location provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
 
+import { createHistory, LocationProvider } from "@reach/router"
+
+let history = createHistory(window)
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <LocationProvider history={history}>
+        <App />
+      </LocationProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
